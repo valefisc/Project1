2f1cd96a94163d79c7fbef94774988868e74b499
Valerie <valerie@valerie-fischer.de>
2022-02-17 19:27:02 +0100

    Überarbeitet

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let rounds = prompt("How many rounds do you wanna play?");
 console.log("Help");
-/*let integer = parseInt(rounds, 10);
+let integer = parseInt(rounds, 10);
 let userWins = 0;
 let computerWins = 0;
 let tie = 0;
-for(let i = 0; i <= integer; i++){
-    let userchoice = prompt ("Rock, paper or scissors? (Use r, p or s)");
+for(let i = 0; i < integer; i++){
+    let userChoice = prompt ("Rock, paper or scissors? (Use r, p or s)");
+    console.log(userChoice) 
     let randomNumber = Math.floor(Math.random() * 3);
+    console.log (randomNumber);
     let computerChoice;
     if (randomNumber === 0) {
         computerChoice = "r";
@@ -15,20 +17,22 @@ for(let i = 0; i <= integer; i++){
     } else {
         computerChoice ="s";
     }
-    if (computerChoice === "r" && userchoice === "p") {
+    console.log (computerChoice);
+    if (computerChoice === "r" && userChoice === "p") {
         userWins++;
         alert ("You won!");
-    } else if (computerChoice === "p" && userchoice === "s"){
+    } else if (computerChoice === "p" && userChoice === "s"){
         userWins++;
         alert ("You won!");
-    } else if (computerChoice === "s" && userchoice === "r"){
+    } else if (computerChoice === "s" && userChoice === "r"){
         userWins++;
         alert ("You won!");
-    } else if (computerChoice === userchoice){
+    } else if (computerChoice === userChoice){
         tie++;
         alert ("It's a tie.");
     } else {
         computerWins++;
         alert ("You lost.");
     }
-}*/
+}
+alert ("Good Game! You won " + userWins + " time(s)! You lost " + computerWins + " time(s) ans tied " + tie + " time(s)!")
