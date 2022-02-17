f41062fcf55af3044334fb804927f07b106a5bca
Valerie <valerie@valerie-fischer.de>
2022-02-17 23:48:20 +0100

    final commit

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ let userWins = 0;
 let computerWins = 0;
 let tie = 0;
 for(let i = 0; i < integer; i++){
-    let userChoice = prompt ("Rock, paper or scissors? (Use r, p or s)");
+    let userChoice = prompt ("Rock, paper or scissors? (Enter r, p or s)");
     if (userChoice === "r" || userChoice === "p" || userChoice === "s"){
         console.log(userChoice);
     } else {
         alert ("Invalid input!!");
+        integer++;
+        continue;
     }
     let randomNumber = Math.floor(Math.random() * 3);
     console.log (randomNumber);
@@ -24,19 +26,19 @@ for(let i = 0; i < integer; i++){
     console.log (computerChoice);
     if (computerChoice === "r" && userChoice === "p") {
         userWins++;
-        alert ("You won!");
+        alert ("You won! " + userChoice + " beats " + computerChoice + ".");
     } else if (computerChoice === "p" && userChoice === "s"){
         userWins++;
-        alert ("You won!");
+        alert ("You won! " + userChoice + " beats " + computerChoice + ".");
     } else if (computerChoice === "s" && userChoice === "r"){
         userWins++;
-        alert ("You won!");
+        alert ("You won! " + userChoice + " beats " + computerChoice + ".");
     } else if (computerChoice === userChoice){
         tie++;
         alert ("It's a tie.");
     } else {
         computerWins++;
-        alert ("You lost.");
+        alert ("You lost. The computer chose " + computerChoice + " which beats " + userChoice + ".");
     }
 }
-alert ("Good Game! You won " + userWins + " time(s)! You lost " + computerWins + " time(s) and tied " + tie + " time(s)!");
+alert ("Good game! You won " + userWins + " time(s)! You lost " + computerWins + " time(s) and tied " + tie + " time(s)!");
